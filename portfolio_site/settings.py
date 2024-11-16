9b81bf250a13847c7e13dfadf5b8982ea6f29653
Adrian Berisha <adrianberisha@MBP-von-Adrian.telekom.ip>
2024-11-16 13:56:26 +0100

    Mediafiles changes

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -126,9 +126,6 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-if not DEBUG:
-    MEDIA_URL = '/uploads/'
-    MEDIA_ROOT = path.join(BASE_DIR, 'uploads')
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/"
