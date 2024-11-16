38e728e1be535d748b580969d336bd57cc78dab5
Adrian Berisha <adrianberisha@MBP-von-Adrian.telekom.ip>
2024-11-16 12:59:08 +0100

    staticfiles

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -22,5 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("portfolio.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
